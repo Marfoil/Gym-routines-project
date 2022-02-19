2c8dfa7d6db7acbf55f2b1c81e461888f4ef1324
Javier Marfil <xavimarfil@gmail.com>
2022-02-19 20:50:50 +0100

    fix: animations

--- FE/src/components/Login/Login.jsx
+++ FE/src/components/Login/Login.jsx
@@ -68,123 +68,120 @@ const Login = props => {
             </div>
 
             {/* NOTE: This is for the sing up section form */}
-            {LoginState && (
-                <div
-                    className={`login__create-container ${
-                        LoginState ? 'login__create-container--active' : 'login__create-container--inactive'
-                    }`}
-                >
-                    Create Account
-                    <span className="login__create-container--info-text">Use email for your registration</span>
-                    <div className="login__create-container__form-container">
+
+            <div
+                className={`login__create-container ${
+                    LoginState ? 'login__create-container--active' : 'login__create-container--inactive'
+                }`}
+            >
+                Create Account
+                <span className="login__create-container--info-text">Use email for your registration</span>
+                <div className="login__create-container__form-container">
+                    <form
+                        className="login__create-container__form-container__form"
+                        onSubmit={e => {
+                            e.preventDefault();
+                            signUp();
+                        }}
+                    >
+                        <input
+                            className="login__create-container__form-container__form--name"
+                            type="text"
+                            placeholder="Name"
+                            value={SignUpForm.name}
+                            onChange={value =>
+                                setSignUpForm({
+                                    ...SignUpForm,
+                                    name: value.target.value,
+                                })
+                            }
+                            required
+                        />
+                        <input
+                            className="login__create-container__form-container__form--email"
+                            type="email"
+                            placeholder="Email"
+                            value={SignUpForm.email}
+                            onChange={value =>
+                                setSignUpForm({
+                                    ...SignUpForm,
+                                    email: value.target.value,
+                                })
+                            }
+                            required
+                        />
+                        <input
+                            className="login__create-container__form-container__form--password"
+                            type="password"
+                            placeholder="Password"
+                            value={SignUpForm.password}
+                            onChange={value =>
+                                setSignUpForm({
+                                    ...SignUpForm,
+                                    password: value.target.value,
+                                })
+                            }
+                            required
+                        />
+                        <button className="login__create-container__form-container__form--submit">Sign Up</button>
+                    </form>
+                </div>
+            </div>
+            {/* NOTE: This is for the log in form */}
+
+            <div
+                className={`login__login-container ${
+                    !LoginState ? 'login__login-container--active' : 'login__login-container--inactive'
+                }`}
+            >
+                <div className="login__login-container__logo-container">
+                    <img className="login__login-container__logo-container--image" src={logo} />
+                    GYM Routines
+                </div>
+                <div className="login__login-container__main-container">
+                    <span className="login__login-container__main-container--info-text">Use email for your login</span>
+                    <div className="login__login-container__main-container__form-container">
                         <form
-                            className="login__create-container__form-container__form"
+                            className="login__login-container__main-container__form-container__form"
                             onSubmit={e => {
                                 e.preventDefault();
-                                signUp();
+                                signIn();
                             }}
                         >
                             <input
-                                className="login__create-container__form-container__form--name"
-                                type="text"
-                                placeholder="Name"
-                                value={SignUpForm.name}
-                                onChange={value =>
-                                    setSignUpForm({
-                                        ...SignUpForm,
-                                        name: value.target.value,
-                                    })
-                                }
-                                required
-                            />
-                            <input
-                                className="login__create-container__form-container__form--email"
+                                className="login__login-container__main-container__form-container__form--email"
                                 type="email"
                                 placeholder="Email"
-                                value={SignUpForm.email}
+                                value={LogInForm.email}
                                 onChange={value =>
-                                    setSignUpForm({
-                                        ...SignUpForm,
+                                    setLogInForm({
+                                        ...LogInForm,
                                         email: value.target.value,
                                     })
                                 }
                                 required
                             />
                             <input
-                                className="login__create-container__form-container__form--password"
+                                className="login__login-container__main-container__form-container__form--password"
                                 type="password"
                                 placeholder="Password"
-                                value={SignUpForm.password}
+                                value={LogInForm.password}
                                 onChange={value =>
-                                    setSignUpForm({
-                                        ...SignUpForm,
+                                    setLogInForm({
+                                        ...LogInForm,
                                         password: value.target.value,
                                     })
                                 }
                                 required
                             />
-                            <button className="login__create-container__form-container__form--submit">Sign Up</button>
+                            <button className="login__login-container__main-container__form-container__form--submit">
+                                Sign In
+                            </button>
                         </form>
                     </div>
                 </div>
-            )}
-            {/* NOTE: This is for the log in form */}
-            {!LoginState && (
-                <div
-                    className={`login__login-container ${
-                        !LoginState ? 'login__login-container--active' : 'login__login-container--inactive'
-                    }`}
-                >
-                    <div className="login__login-container__logo-container">
-                        <img className="login__login-container__logo-container--image" src={logo} />
-                        GYM Routines
-                    </div>
-                    <div className="login__login-container__main-container">
-                        <span className="login__login-container__main-container--info-text">
-                            Use email for your login
-                        </span>
-                        <div className="login__login-container__main-container__form-container">
-                            <form
-                                className="login__login-container__main-container__form-container__form"
-                                onSubmit={e => {
-                                    e.preventDefault();
-                                    signIn();
-                                }}
-                            >
-                                <input
-                                    className="login__login-container__main-container__form-container__form--email"
-                                    type="email"
-                                    placeholder="Email"
-                                    value={LogInForm.email}
-                                    onChange={value =>
-                                        setLogInForm({
-                                            ...LogInForm,
-                                            email: value.target.value,
-                                        })
-                                    }
-                                    required
-                                />
-                                <input
-                                    className="login__login-container__main-container__form-container__form--password"
-                                    type="password"
-                                    placeholder="Password"
-                                    value={LogInForm.password}
-                                    onChange={value =>
-                                        setLogInForm({
-                                            ...LogInForm,
-                                            password: value.target.value,
-                                        })
-                                    }
-                                    required
-                                />
-                                <button className="login__login-container__main-container__form-container__form--submit">
-                                    Sign In
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            )}
+            </div>
+
             {/* NOTE: This is for the right menu of the log in section (pink) */}
 
             <div
